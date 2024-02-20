3b11f69b2c769b793aa1c997452349218b80558c
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-20 14:57:28 +0530

    second commit

--- calc.java
+++ calc.java
@@ -1,1 +1,15 @@
-#Assignment_1
+import java.util.*;
+import java.io;
+
+public class calc{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+
+        add();
+        sub();
+    }
+}
